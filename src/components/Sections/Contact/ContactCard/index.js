2189ffe13a620a9bfd996b6a62f6b06c180f0a59
Gkristof96 <kristef.g@gmail.com>
2021-01-12 12:31:55 +0100

    contactcard done

--- src/components/Sections/Contact/ContactCard/index.js
+++ src/components/Sections/Contact/ContactCard/index.js
@@ -3,7 +3,25 @@ import React from 'react'
 const ContactCard = () => {
     return (
         <>
-            micimackó
+            <div className='contact-card'>
+                <h1>Do you have a question?</h1>
+                <h2>Complete this form, and we will answer it!</h2>
+                <form>
+                    <div className='input-group'>
+                        <label>Name</label>
+                        <input type="text" placeholder='Sebastion Montgomeri' />
+                    </div>
+                    <div className='input-group'>
+                        <label>Email</label>
+                        <input type="email" placeholder='sebastion@mail.com' />
+                    </div>
+                    <div className='input-group'>
+                        <label>Name</label>
+                        <textarea />
+                    </div>
+                    <button type='submit'>Send</button>
+                </form>
+            </div>
         </>
     )
 }
