7d4c40886d15aa45d017fa6d948dc02efc060705
Gkristof96 <kristef.g@gmail.com>
2021-04-18 12:02:43 +0200

    responsive issues fixed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import './App.css';
-import './css/style.css'
-import Header from './components/Header'
-import Hero from './components/Sections/Hero'
-import Services from './components/Sections/Services'
-import Trainers from './components/Sections/Trainers'
-import Prices from './components/Sections/Prices'
-import Contact from './components/Sections/Contact';
-import Footer from './components/Footer'
-import { sectionData } from './data/SectionData'
+import React from "react";
+import "./App.css";
+import "./css/style.css";
+import Header from "./components/Header";
+import Hero from "./components/Sections/Hero";
+import Services from "./components/Sections/Services";
+import Trainers from "./components/Sections/Trainers";
+import Prices from "./components/Sections/Prices";
+import Contact from "./components/Sections/Contact";
+import Footer from "./components/Footer";
+import { sectionData } from "./data/SectionData";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <div className='filler light' />
-      <Services data={sectionData[0]}/>
-      <div className='filler dark' />
+      <div className="filler light" />
+      <Services data={sectionData[0]} />
+      <div className="filler dark" />
       <Trainers />
-      <div className='filler light' />
-      <Prices data={sectionData[1]}/>
-      <div className='filler dark' />
-      <Contact data={sectionData[2]}/>
+      <div className="filler light" />
+      <Prices data={sectionData[1]} />
+      <div className="filler dark" />
+      <Contact data={sectionData[2]} />
       <Footer />
     </>
   );
